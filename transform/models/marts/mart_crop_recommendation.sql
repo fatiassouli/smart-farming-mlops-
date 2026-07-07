a4b40b6496b51c323ababcc1169163d2f7011014
OuarrakLayla <ouarraklayla@gmail.com>
2026-07-07 22:22:57 +0200

    US-03: ajout du feature engineering (npk_total, ratios, interactions, year_norm, log_yield)

--- transform/models/marts/mart_crop_recommendation.sql
+++ transform/models/marts/mart_crop_recommendation.sql
@@ -6,5 +6,11 @@ select
     humidity,
     ph,
     rainfall,
-    crop_label
+    crop_label,
+    -- Feature engineering (aligné avec le script d'inférence de Person 5)
+    nitrogen + phosphorus + potassium as npk_total,
+    nitrogen / (phosphorus + 1e-6) as np_ratio,
+    nitrogen / (potassium + 1e-6) as nk_ratio,
+    (temperature * humidity) / 100 as heat_humidity,
+    rainfall * temperature as rain_temp
 from {{ ref('stg_crop_recommendation') }}
