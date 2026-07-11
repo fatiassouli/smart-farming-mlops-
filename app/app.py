9422476390436f28a20e99d4aa0ed49787720fc3
SANAE OUTIGHLI <sanaeoutighli122@gmail.com>
2026-07-11 21:57:30 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -464,7 +464,7 @@ def model_status():
 
 if __name__ == "__main__":
     print("🌱 AgriSmart démarré sur http://127.0.0.1:5000")
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=5000)
 
 
 # ── Effacer l'historique ────────────────────────────────────────
